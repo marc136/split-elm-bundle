1261c223dca2970308b1723a2615a5bc68f317e0
Marc Walter <walter.marc@outlook.com>
2024-01-28 13:32:06 +0100

    Fix inlined test data

--- src/compiled.test.mjs
+++ src/compiled.test.mjs
@@ -17,8 +17,8 @@ test('compile BrowserElement.elm', async () => {
     const result = await wipAnalyze('examples/from-aide/compiled/BrowserElement.js')
     expect(result).toMatchInlineSnapshot(`
       {
-        "esmByteSize": 71194,
-        "esmChars": 71194,
+        "esmByteSize": 71096,
+        "esmChars": 71096,
         "iifeByteSize": 113593,
         "iifeChars": 113593,
       }
@@ -29,8 +29,8 @@ test('compile BrowserApplication.elm', async () => {
     const result = await wipAnalyze('examples/from-aide/compiled/BrowserApplication.debug.js')
     expect(result).toMatchInlineSnapshot(`
       {
-        "esmByteSize": 84582,
-        "esmChars": 84582,
+        "esmByteSize": 84484,
+        "esmChars": 84484,
         "iifeByteSize": 280011,
         "iifeChars": 279991,
       }
@@ -41,8 +41,8 @@ test('compile BrowserSandbox+BrowserElement.elm', async () => {
     const result = await wipAnalyze('examples/from-aide/compiled/BrowserSandbox+BrowserElement.js')
     expect(result).toMatchInlineSnapshot(`
       {
-        "esmByteSize": 74134,
-        "esmChars": 74134,
+        "esmByteSize": 74036,
+        "esmChars": 74036,
         "iifeByteSize": 116487,
         "iifeChars": 116487,
       }
