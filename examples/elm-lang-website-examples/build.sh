f3d001822c93c3bfe6efcddf20064ca22746b7f5
Marc Walter <walter.marc@outlook.com>
2024-03-02 07:26:31 +0100

    Add more explanation to the example page and add it to the README

--- examples/elm-lang-website-examples/build.sh
+++ examples/elm-lang-website-examples/build.sh
@@ -1,7 +1,13 @@
 #!/bin/env bash
 
 # compile the js bundle
-elm make --optimize --output=www/examples.js src/Animation.elm src/Clock.elm src/Cube.elm src/Mouse.elm src/Quotes.elm src/TextField.elm 
+elm make --optimize --output=www/examples.js \
+    src/Animation.elm \
+    src/Clock.elm \
+    src/Cube.elm \
+    src/Mouse.elm \
+    src/Quotes.elm \
+    src/TextField.elm 
 
 # split the bundle
 node ../../bin/split-elm-bundle.mjs www/examples.js
